bb9f7a072b95b8d669ff7d36beacb88d5fc45b0a
Travenedu <82552048+Travenedu@users.noreply.github.com>
2025-10-19 01:38:25 -0400

    Update settings

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -2,20 +2,33 @@ vim.loader.enable()
 
 local opt = vim.opt
 
--- better indentation
+-- Search
+opt.ignorecase = true -- Ignore case
+opt.smartcase = true -- Don't ignore case with capitals
+opt.hlsearch = true -- Don't highlight search results
+opt.incsearch = true -- Incremental search
+opt.grepprg = "rg --vimgrep"
+opt.grepformat = "%f:%l:%c:%m"
+
+-- Indentation
+opt.expandtab = true -- Use spaces instead of tabs
 opt.tabstop = 2
 opt.softtabstop = 2
 opt.shiftwidth = 2
-opt.expandtab = true
-opt.smartindent = true
 opt.autoindent = true
+opt.smartindent = true -- Insert indents automatically
+opt.shiftround = true -- Round indent
 
-opt.autowrite = true -- Enable auto write
-opt.completeopt = "menu,menuone,noselect"
-opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
+-- Appearance
+opt.termguicolors = true -- True color support
+opt.number = true -- Print line number
 opt.cursorline = true -- Enable highlighting of the current line
-opt.expandtab = true -- Use spaces instead of tabs
+opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+opt.laststatus = 3 -- global statusline
+opt.ruler = true -- Enable the ruler
+opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
+opt.pumblend = 10 -- Popup blend
+opt.winminwidth = 5 -- Minimum window width
 opt.fillchars = {
   foldopen = "",
   foldclose = "",
@@ -24,65 +37,38 @@ opt.fillchars = {
   diff = "╱",
   eob = " ",
 }
-opt.foldlevel = 99
-opt.grepformat = "%f:%l:%c:%m"
-opt.grepprg = "rg --vimgrep"
-opt.ignorecase = true -- Ignore case
-opt.inccommand = "nosplit" -- preview incremental substitute
-opt.jumpoptions = "view"
-opt.laststatus = 3 -- global statusline
-opt.list = true -- Show some invisible characters (tabs...
-opt.number = true -- Print line number
-opt.pumblend = 10 -- Popup blend
-opt.pumheight = 10 -- Maximum number of entries in a popup
-opt.relativenumber = true -- Relative line numbers
-opt.ruler = false -- Disable the default ruler
-opt.scrolloff = 4 -- Lines of context
-opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
-opt.shiftround = true -- Round indent
-opt.shiftwidth = 2 -- Size of an indent
-opt.shortmess:append({ W = true, I = true, c = true, C = true })
-opt.sidescrolloff = 8 -- Columns of context
-opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true -- Don't ignore case with capitals
-opt.smartindent = true -- Insert indents automatically
-opt.spelllang = { "en" }
 
--- term colors
-opt.termguicolors = true -- True color support
-opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
-opt.winminwidth = 5 -- Minimum window width
-
--- folds
-opt.foldenable = false
-opt.foldmethod = "manual"
-
--- wrap
+-- Behavior
+opt.autowrite = true -- Enable auto write
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.undofile = true -- Save undo history
 opt.wrap = false
-opt.linebreak = true
-opt.breakat = " "
-opt.textwidth = 80
-
--- remove eof character
-opt.fillchars = { eob = " " }
-
--- Save undo history
-opt.undofile = true
-
--- incremental search
-opt.hlsearch = false
-opt.incsearch = true
-
--- fast update
-opt.updatetime = 50
-
--- Enable break indent
 opt.breakindent = true
-
--- Split direction
 opt.splitright = true
-opt.splitkeep = "screen"
 opt.splitbelow = true
+opt.splitkeep = "screen"
+opt.scrolloff = 4 -- Lines of context
+opt.sidescrolloff = 8 -- Columns of context
+opt.list = true -- Show some invisible characters (tabs...
+opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+opt.jumpoptions = "view"
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
 
--- Fix markdown indentation settings
-vim.g.markdown_recommended_style = 0
+-- Folds
+opt.foldenable = true
+opt.foldmethod = "manual"
+opt.foldlevel = 99
+
+-- Performance
+opt.updatetime = 300 -- Faster update time for plugins
+opt.shortmess:append({ W = true, I = true, c = true, C = true })
+
+-- Completion
+opt.completeopt = "menu,menuone,noselect"
+opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.inccommand = "nosplit" -- preview incremental substitute
+
+
+-- Language specific
+opt.spelllang = { "en" }
+vim.g.markdown_recommended_style = 0 -- Fix markdown indentation settings
