3ab7416415c569a99daf3f89faa87d160307d4b5
Travenedu <82552048+Travenedu@users.noreply.github.com>
2025-12-10 23:06:31 -0500

    remove fzf-lua picker in favor of snacks picker for lsp

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,5 +1,9 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
 
@@ -10,11 +14,11 @@ return {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.completion.spell,
-        -- null_ls.builtins.diagnostics.eslint_d,
+        require("none-ls.diagnostics.eslint_d"),
       },
     })
 
-    vim.keymap.set("n", "<leader>mm", function()
+    vim.keymap.set("n", "<leader>,,", function()
       vim.lsp.buf.format()
       vim.notify("File formatted 🔔")
     end, { desc = "Format file" })
