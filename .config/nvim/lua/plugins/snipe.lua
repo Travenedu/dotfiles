1e5d6cd99e735f80a02db39e01dfb50692e08214
Travenedu <82552048+Travenedu@users.noreply.github.com>
2025-12-10 23:13:43 -0500

    minor adjustments

--- .config/nvim/lua/plugins/snipe.lua
+++ .config/nvim/lua/plugins/snipe.lua
@@ -6,13 +6,13 @@ return {
       },
       config = function()
         local snipe = require("snipe")
-  
+
         -- Setup
         snipe.setup()
-  
+ 
         -- Optional: another keybind (if you want a leader-prefixed one)
         vim.keymap.set("n", "gb", snipe.open_buffer_menu, {desc = 'Open Snipe File Buffer'})
-        
+ 
         -- UI customizations
         snipe.ui_select_menu = require("snipe.menu"):new { position = "center" }
         snipe.ui_select_menu:add_new_buffer_callback(function(m)
@@ -20,10 +20,9 @@ return {
             m:close()
           end, { nowait = true, buffer = m.buf })
         end)
-  
+ 
         -- Override vim.ui.select
         vim.ui.select = snipe.ui_select
       end,
     }
 }
-  
