0fc4695e0808a9bb4e9d798e7206d7322219d150
Travenedu <82552048+Travenedu@users.noreply.github.com>
2025-12-10 23:07:18 -0500

    Add some autocommands

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -45,7 +45,6 @@ opt.undofile = true -- Save undo history
 opt.wrap = false
 opt.breakindent = true
 opt.splitright = true
-opt.splitbelow = true
 opt.splitkeep = "screen"
 opt.scrolloff = 4 -- Lines of context
 opt.sidescrolloff = 8 -- Columns of context
@@ -53,6 +52,8 @@ opt.list = true -- Show some invisible characters (tabs...
 opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
 opt.jumpoptions = "view"
 opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+opt.synmaxcol = 200  -- don't highlight lines longer than 200 chars
+vim.g.syntax_timeout = 1000  -- timeout after 1 second
 
 -- Folds
 opt.foldenable = true
@@ -68,7 +69,26 @@ opt.completeopt = "menu,menuone,noselect"
 opt.pumheight = 10 -- Maximum number of entries in a popup
 opt.inccommand = "nosplit" -- preview incremental substitute
 
-
 -- Language specific
 opt.spelllang = { "en" }
 vim.g.markdown_recommended_style = 0 -- Fix markdown indentation settings
+
+-- Auto commands
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "help",
+  command = "wincmd L",
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  pattern = "*",
+  desc = "highlight selction on yank",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200, visual = true})
+  end,
+})
+
+-- auto resize splits when terminal windown is resize
+vim.api.nvim_create_autocmd("VimResized", {
+  command = "wincmd =",
+})
