93f8d2bf6841d58c16b9624a1546c9af0c470303
Travenedu <82552048+Travenedu@users.noreply.github.com>
2025-12-28 00:01:05 -0500

    Update persistance plugin

--- .config/nvim/lua/plugins/persistence.lua
+++ .config/nvim/lua/plugins/persistence.lua
@@ -1,27 +1,46 @@
 return {
-  "folke/persistence.nvim",
-  event = "BufReadPost", -- Start persistence when a buffer is read
-  module = "persistence",
-  config = function()
-    require("persistence").setup({
-      -- Optional: Configure the directory where sessions are saved
-      dir = vim.fn.stdpath("data") .. "/sessions/",
-      -- Optional: List of file types you DON'T want to save sessions for
-      exclude_ft = { "gitcommit", "gitrebase", "gitmessenger", "telescope", "man" },
-      -- Optional: Automatically load the last session on Neovim startup
-      autoload = {
-        enabled = true,
-        -- You may want to disable this if you use other session managers
-        -- command = 'RestoreSession', 
-      },
-      -- Optional: List of commands to be executed when a session is loaded.
-      -- post_restore = { "cd " .. vim.fn.getcwd() },
-    })
-  end,
+	"folke/persistence.nvim",
+	event = "BufReadPre", -- Load slightly earlier to ensure session state is ready
+	opts = {
+		-- Default directory is already ~/.local/share/nvim/sessions/
+		-- Only change 'dir' if you want sessions saved elsewhere
+		options = { "buffers", "curdir", "tabpages", "winsize", "skiprtp" },
+	},
+	keys = {
+		-- Restore the session for the current directory
+		{
+			"<leader>qs",
+			function()
+				require("persistence").load()
+			end,
+			desc = "Restore Session (CWD)",
+		},
 
-  keys = {
-    { "<leader>qs", function() require("persistence").load() end, desc = "Restore last session" },
-    { "<leader>ql", function() require("persistence").load() end, desc = "Load session" },
-    { "<leader>qd", function() require("persistence").stop() end, desc = "Delete current session" },
-  },
+		-- Select a session to load from a list
+		{
+			"<leader>qS",
+			function()
+				require("persistence").select()
+			end,
+			desc = "Select Session",
+		},
+
+		-- Restore the last session (regardless of CWD)
+		{
+			"<leader>ql",
+			function()
+				require("persistence").load({ last = true })
+			end,
+			desc = "Restore Last Session",
+		},
+
+		-- Stop Persistence (session won't be saved on exit)
+		{
+			"<leader>qd",
+			function()
+				require("persistence").stop()
+			end,
+			desc = "Don't Save Current Session",
+		},
+	},
 }
