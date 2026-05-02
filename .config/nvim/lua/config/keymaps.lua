180d38e590d2a8b814481d1b2302c715d8cac41a
Travenedu <82552048+Travenedu@users.noreply.github.com>
2026-05-02 01:12:43 -0400

    feat(keymaps): map <leader>qr to neovim restart
    This is being done because of preference

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -87,4 +87,4 @@ vim.keymap.set("n", "<leader>cd", lsp_utils.copy_diagnostic, { desc = "Copy Diag
 -- vim.keymap.set("n", "gs", lsp_utils.toggle_hover_vertical, { desc = "Toggle Vertical Docs" })
 
 -- System
-map("n", "<leader>qR", "<cmd>restart<cr>", "Quit and Restart Neovim")
+map("n", "<leader>qr", "<cmd>restart<cr>", "Quit and Restart Neovim")
